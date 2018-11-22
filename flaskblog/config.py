147e0c1dc94b9da79b561cdb72bcfbd8700f2d09
Ratchapoom <Ratchapoom.k@kkumail.com>
2018-11-23 00:19:40 +0700

    fixed #1

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -8,6 +8,6 @@ class Config:
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS')
     MAIL_USERNAME = os.environ.get('EMAIL_USER')
     MAIL_PASSWORD = os.environ.get('EMAIL_PASS')
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = os.environ.get(
         'SQLALCHEMY_TRACK_MODIFICATIONS')
