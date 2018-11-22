79701ae8465fd537dc6798ee5c0ee0246868e4e2
Ratchapoom <Ratchapoom.k@kkumail.com>
2018-11-22 20:50:14 +0700

    config

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -3,9 +3,12 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
-    MAIL_SERVER = 'smtp.googlemail.com'
-    MAIL_PORT = 587
-    MAIL_USE_TLS = True
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = os.environ.get('MAIL_PORT')
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS')
     MAIL_USERNAME = os.environ.get('EMAIL_USER')
     MAIL_PASSWORD = os.environ.get('EMAIL_PASS')
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://{user}:{pw}@{url}/{db}'.format(
+        user=os.environ.get('POSTGRES_USER'), pw=os.environ.get("POSTGRES_PW"), url=os.environ.get("POSTGRES_URL"), db=os.environ.get("POSTGRES_DB"))
+    SQLALCHEMY_TRACK_MODIFICATIONS = os.environ.get(
+        'SQLALCHEMY_TRACK_MODIFICATIONS')
